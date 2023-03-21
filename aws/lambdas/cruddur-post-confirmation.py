5dff27c5d8d887d645a9632c4851294a8439868b
Dsar-gh <125710134+Dsar-gh@users.noreply.github.com>
2023-03-21 03:39:32 +0100

    Update cruddur-post-confirmation.py

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -1,48 +1,54 @@
-import os
 import json
 import psycopg2
-
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
-    user_display_name = user["name"]
-    user_email = user["email"]
-    user_handle = user["preferred_username"]
-    user_cognito_id = user["sub"]
+    print('userAttributes')
+    print(user)
 
+    user_display_name           = user['name']
+    user_email                  = user['email']
+    user_handle                 = user['preferred_username']
+    user_cognito_id             = user['sub']
     try:
-        conn = psycopg2.connect(os.getenv("CONNECTION_URL"))
-        cur = conn.cursor()
-        sql = f"""
-        INSERT INTO public.users (
-            display_name,
-            email, 
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
+            display_name, 
+            email,
             handle, 
             cognito_user_id
-            ) 
+        ) 
         VALUES(
-            %s, %s, %s, %s
-            )
-        """
-        args = [
-            user_display_name,
-            user_email,
-            user_handle,
-            user_cognito_id
-        ]
+            %(display_name)s,
+            %(email)s,
+            %(handle)s,
+            %(cognito_id)s
+        )
+      """
+      
+      print('SQL Statement ----')
+      print(sql)
 
-        print(sql)
-        cur.execute(sql, *args)
-        conn.commit()
-        print('Commit Done')
+      params = {
+        'display_name': user_display_name,
+        'email': user_email,
+        'handle': user_handle,
+        'cognito_id': user_cognito_id
+      }
+      
+      
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      cur.execute(sql,params)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
+      print(error)
     finally:
-        if conn is not None:
-            cur.close()
-            conn.close()
-            print('Database connection closed.')
-
+      if conn is not None:
+        cur.close()
+        conn.close()
+        print('Database connection closed.')
     return event
